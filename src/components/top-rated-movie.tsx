24c9c1a37211107ca64701ce56bf4acb62c8448c
Aayush Raut <aayussraut.ar@gmail.com>
2024-04-23 19:56:54 +0545

    refactor: fix typo

--- src/components/top-rated-movie.tsx
+++ src/components/top-rated-movie.tsx
@@ -1,12 +1,12 @@
 import { fetchTopRatedMovies } from "@/actions/fetch-top-rated-movies";
 
-import TopRatedAndUpcommingMovieSlider from "./slider/top-rated-and-upcomming-slider";
+import TopRatedAndUpcomingMovieSlider from "./slider/top-rated-and-upcoming-slider";
 
 const TopRatedMovie = async () => {
   const movies = await fetchTopRatedMovies();
   return (
     <div className="">
-      <TopRatedAndUpcommingMovieSlider title="Top Rated" movies={movies} />
+      <TopRatedAndUpcomingMovieSlider title="Top Rated" movies={movies} />
     </div>
   );
 };
