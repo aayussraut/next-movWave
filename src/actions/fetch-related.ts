307309b40fb4cb2f99dad9a4f25fc22801ac9778
Aayush Raut <aayussraut.ar@gmail.com>
2024-04-27 20:08:05 +0545

    feat: add action to fetch related movie or tvshows

---
+++ src/actions/fetch-related.ts
@@ -0,0 +1,21 @@
+"use server";
+import axios from "axios";
+
+export const fetchRelated = async (mediaId: string, mediaType: string) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/${mediaType}/${mediaId}/similar`,
+      {
+        params: {
+          api_key: `${process.env.API_KEY}`,
+        },
+      }
+    );
+
+    // console.log("Response", response.data.results.results);
+
+    return response.data.results;
+  } catch (error) {
+    console.error("Error fetching now showing movies:", error);
+  }
+};
