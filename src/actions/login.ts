cd6e5424da428f791626782bcf27255af16fd654
Aayush Raut <aayussraut.ar@gmail.com>
2024-05-30 08:14:28 +0545

    feat: Implement server action to check if the provided data is in db for login functionality

---
+++ src/actions/login.ts
@@ -0,0 +1,27 @@
+"use server";
+import * as z from "zod";
+import { LoginSchema } from "../../schema";
+import { getUserByEmail } from "../../data/user";
+
+export const login = async (values: z.infer<typeof LoginSchema>) => {
+  const validatedFields = LoginSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid Fields!" };
+  }
+
+  const { email, password, code } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (!existingUser) {
+    return { error: "Invalid Credentials!" };
+  }
+
+  if (existingUser.password !== password) {
+    return { error: "Invalid Credentials!" };
+  }
+
+  console.log("User logged in!");
+  return { success: "User logged in!" };
+};
