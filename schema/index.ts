ce42c5ea857f8196ad26be9fc96215423a78b9ae
Aayush Raut <aayussraut.ar@gmail.com>
2024-05-30 08:08:54 +0545

    feat: Add validation schema for registration form

---
+++ schema/index.ts
@@ -0,0 +1,11 @@
+import * as z from "zod";
+
+export const RegisterSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+  name: z.string().min(1, {
+    message: "Username is required",
+  }),
+});
