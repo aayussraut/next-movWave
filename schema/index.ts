ede357c0124a9ed46d46fac6e62a0ac55f664a31
Aayush Raut <aayussraut.ar@gmail.com>
2024-05-30 08:13:45 +0545

    feat: Add login validation schema

--- schema/index.ts
+++ schema/index.ts
@@ -9,3 +9,11 @@ export const RegisterSchema = z.object({
     message: "Username is required",
   }),
 });
+
+export const LoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(1, {
+    message: "Password is required",
+  }),
+  code: z.string().optional(),
+});
