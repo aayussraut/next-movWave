217062b3bf0d8c6e17dfb4738fc97b220ec8d114
Aayush Raut <aayussraut.ar@gmail.com>
2024-05-30 08:11:35 +0545

    feat: Implement server action to store theuser data in db

---
+++ src/actions/register.ts
@@ -0,0 +1,34 @@
+"use server";
+import * as z from "zod";
+import { RegisterSchema } from "../../schema";
+import { getUserByEmail } from "../../data/user";
+import { db } from "../../lib/db";
+
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  console.log(validatedFields);
+  if (!validatedFields.success) {
+    console.log("Invalid Fields!");
+    return { error: "Invalid Fields!" };
+  }
+
+  const { email, password, name } = validatedFields.data;
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    console.log("User already exists!");
+    return { error: "User already exists!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password,
+    },
+  });
+
+  console.log("User created!");
+  return { success: "User created!" };
+};
