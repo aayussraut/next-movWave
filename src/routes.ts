bb027b87f8e02ec246b59060782045c929d29330
Aayush Raut <aayussraut.ar@gmail.com>
2024-05-31 07:45:10 +0545

    feat: Add public and auth routes

---
+++ src/routes.ts
@@ -0,0 +1,11 @@
+export const publicRoutes = [
+  "/",
+  "/movies",
+  "/tv-shows",
+  "/search-results/",
+  //   /^\/movies\/[^/]+$/, // for dynamic routes
+];
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+export const apiAuthPrefix = "/api/auth";
