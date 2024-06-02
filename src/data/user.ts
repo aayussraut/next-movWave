babc2eba24e466e72c8ac235b3ff91e0a6e1ac5c
Aayush Raut <aayussraut.ar@gmail.com>
2024-06-02 20:25:39 +0545

    feat: Refactor getUserByEmail to use findUnique instead of findFirst

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,9 +1,13 @@
 import { db } from "../lib/db";
 
 export const getUserByEmail = async (email: string) => {
-  return await db.user.findFirst({
-    where: {
-      email,
-    },
-  });
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  } catch (e) {}
 };
