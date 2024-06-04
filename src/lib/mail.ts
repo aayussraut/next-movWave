b160e74e50407ff3aa7b777e1aef4ef9d434b8c8
Aayush Raut <aayussraut.ar@gmail.com>
2024-06-04 08:37:16 +0545

    refactor: Refactor sendMail function to handle email verification and sending in separate steps

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -9,6 +9,19 @@ const transporter = nodeMailer.createTransport({
 });
 
 export const sendMail = async (to: string, subject: string, text: string) => {
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailOptions = {
     from: process.env.EMAIL,
     to,
@@ -17,7 +30,18 @@ export const sendMail = async (to: string, subject: string, text: string) => {
   };
 
   try {
-    const info = await transporter.sendMail(mailOptions);
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          console.log(info);
+          resolve(info);
+        }
+      });
+    });
   } catch (error) {
     console.error("Error sending email:", error);
   }
