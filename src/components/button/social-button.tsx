bea0bbb8e72f3f297115d46cbd611e4ba4e016f2
Aayush Raut <aayussraut.ar@gmail.com>
2024-06-07 11:13:09 +0545

    feat: Add GitHub login support to SocialButton component

--- src/components/button/social-button.tsx
+++ src/components/button/social-button.tsx
@@ -3,7 +3,7 @@ import { signIn } from "next-auth/react";
 import { Button } from "../ui/button";
 
 export const SocialButton = () => {
-  const onClick = (provider: "google" | "facebook") => {
+  const onClick = (provider: "google" | "facebook" | "github") => {
     signIn(provider, {
       callbackUrl: "/",
     });
@@ -17,7 +17,12 @@ export const SocialButton = () => {
       >
         Facebook
       </Button>
-      <Button className="bg-[#1da1f2] text-white">Twitter</Button>
+      <Button
+        className="bg-[#1da1f2] text-white"
+        onClick={() => onClick("github")}
+      >
+        Github
+      </Button>
       <Button
         className="bg-[#fbbc05] text-white"
         onClick={() => onClick("google")}
